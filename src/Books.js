b60a619e3e998a38c972dce870b373c5fe71de49
Jess Doyle <jessica.e.doyle@gmail.com>
2019-08-05 14:05:51 -0400

    working on book

--- src/Books.js
+++ src/Books.js
@@ -9,14 +9,21 @@ function Books(props) {
     console.log(props);
     const books = props.bookdata;
 
+    if (props.bookdata.length >0) {
     const book = books[0];
     console.log(book);
-    let title = book.volumeInfo;
-    let author;
-    let price;
+    let title = book.volumeInfo.title;
+    let authors = book.volumeInfo.authors.join(', ');
+    
+    let price = book.saleInfo;
+    console.log(price);
     let description;
     let thumbnail;
     let id;
+
+    }
+
+    
 
   return (
     <div></div>
